e5501c2a335614aec0aaf01d1c2b7328c897f269
KaleoFeng <kaleofeng@gmail.com>
2019-05-25 16:33:09 +0800

    Docker mysql with mysql_native_password

--- docker/chili/mysql/docker.sh
+++ docker/chili/mysql/docker.sh
@@ -1,1 +1,1 @@
-docker run --name mysql_era --network net_db -p 3306:3306 -v $PWD/conf:/etc/mysql -v $PWD/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Root123456 -d --restart always mysql:8.0.13
+docker run --name mysql_era --network net_db -p 3306:3306 -v $PWD/conf:/etc/mysql -v $PWD/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Root123456 -d --restart always mysql:8.0.13 --default-authentication-plugin=mysql_native_password
