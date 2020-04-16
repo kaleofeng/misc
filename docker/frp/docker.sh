f7ba2b600739e5426feeab2b8a11d5733b8c6cd7
KaleoFeng <kaleofeng@gmail.com>
2020-04-16 22:18:02 +0800

    Frp docker scripts and configs

---
+++ docker/frp/docker.sh
@@ -0,0 +1,13 @@
+docker run \
+--name frps \
+--network frp \
+-p 50000:50000 \
+-p 50500:50500 \
+-p 50080:50080 \
+-p 50443:50443 \
+-p 50022:50022 \
+-p 50122:50122 \
+-v /data/docker/frp/conf/frps.ini:/etc/frp/frps.ini  \
+-d \
+--restart=always  \
+snowdreamtech/frps
