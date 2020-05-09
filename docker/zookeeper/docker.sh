dfa5fa03df541a7952aa5a5aa33f76f9f6c0798b
KaleoFeng <kaleofeng@gmail.com>
2020-05-09 12:16:34 +0800

    Docker zookeeper configs and scripts

--- docker/zookeeper/docker.sh
+++ docker/zookeeper/docker.sh
@@ -1,7 +1,7 @@
 docker run \
   --name zookeeper \
   --network zookeeper \
-  --restart=always  \
+  --restart always  \
   -d \
   -p 2181:2181 \
   -v /data/docker/zookeeper/conf/zoo.cfg:/conf/zoo.cfg  \
