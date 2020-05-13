d63265da39913c46e8c012cf04ee4b0422a116fc
KaleoFeng <kaleofeng@gmail.com>
2020-05-13 22:08:05 +0800

    Weibo wbcrawl sql scripts

---
+++ weibo/wbcrawl/sql/tab_posts.sql
@@ -0,0 +1,43 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : L P cent - cent.metazion.int
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : cent.metazion.int:3306
+ Source Schema         : tool
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 13/05/2020 22:05:45
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tab_posts
+-- ----------------------------
+DROP TABLE IF EXISTS `tab_posts`;
+CREATE TABLE `tab_posts`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pid` bigint(20) NOT NULL,
+  `ptext` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` int(10) NOT NULL,
+  `reposts_count` int(10) NOT NULL,
+  `comments_count` int(10) NOT NULL,
+  `attitudes_count` int(10) NOT NULL,
+  `author_id` bigint(20) NOT NULL,
+  `author_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `urank` smallint(5) NOT NULL,
+  `followers_count` int(10) NOT NULL,
+  `follow_count` int(10) NOT NULL,
+  `record_time` int(10) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique_pid`(`pid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
