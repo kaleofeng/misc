3b1c41f8f78fb397174e86900e6afceec0b7f3f9
KaleoFeng <kaleofeng@gmail.com>
2020-05-16 23:18:17 +0800

    Docket mysql configs and scripts

--- docker/mysql/docker.sh
+++ docker/mysql/docker.sh
@@ -2,10 +2,10 @@ docker run \
   --name mysql_era \
   --network net_db \
   --restart always \
-  --default-authentication-plugin=mysql_native_password \
   -d \
   -p 3306:3306 \
   -v $PWD/conf:/etc/mysql \
   -v $PWD/data:/var/lib/mysql \
   -e MYSQL_ROOT_PASSWORD=Root123456 \
-  mysql:8.0.13
+  mysql:8.0.13 \
+  --default-authentication-plugin=mysql_native_password
