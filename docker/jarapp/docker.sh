57a3f2414edbb356a19d3102a84f133cd56ad887
KaleoFeng <kaleofeng@gmail.com>
2020-11-01 14:17:15 +0800

    Trivial things

--- docker/jarapp/docker.sh
+++ docker/jarapp/docker.sh
@@ -1,9 +1,9 @@
 docker run \
-  --name app_era \
+  --name jarapp_official_era \
   --restart=always \
   --publish 8081:8080 \
   --volume $PWD/bin:/data/bin \
   --network net_web \
   --detach \
   kaleofeng/jarapp:1.0-SNAPSHOT \
-  java -Xms64m -Xmx64m -XX:MaxPermSize=64m -jar ./app.jar
+  java -Xms64m -Xmx64m -jar ./official.jar
