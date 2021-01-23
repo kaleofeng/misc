4406ea6203dbe54043adb554ba66f6646bf3e56c
kaleofeng <kaleofeng@gmail.com>
2021-01-23 18:29:15 +0800

    Shell script rename filenames in batch

--- tool/rename/rename.sh
+++ tool/rename/rename.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-ls | sed -r -n "s#(.*)(\.\w+)#mv '\1\2' '\1'#gp" | sh
+
+if [ $# -ne 3 ]; then
+    printf "Rename filenames in batch\n"
+    printf "Usage: rename [src] [dst] [dir]\n"
+    printf ".e.g.: rename hallo hello ."
+    exit 0
+fi
+
+src=$1
+dst=$2
+dir=$3
+
+ls $dir | awk "{x=\$0; gsub(/$src/, \"$dst\"); print \"mv \"x\" \"\$0}" | sh
