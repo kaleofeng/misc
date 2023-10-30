3c1a325862218bfd22412d00508f4552908b896f
kaleofeng <kaleofeng@gmail.com>
2023-10-30 22:23:03 +0800

    Gen English vacabulary excel based on Bing Dict

---
+++ scraper/bing/bing_dict.py
@@ -0,0 +1,113 @@
+import random
+import re
+import requests
+from bs4 import BeautifulSoup
+
+class WordInfo(object):
+    def __init__(self, word):
+        self.word = word
+        self.pronunciations = []
+        self.definitions = []
+        self.examples = []
+
+    def word_text(self):
+        return self.word
+    
+    def pronunciation_text(self):
+        ps = []
+        for p in self.pronunciations:
+            vs = [value for value in p.values()]
+            ps.extend(vs)
+        return '/{}/'.format('; '.join(ps))
+
+    def definition_text(self):
+        ds = []
+        for d in self.definitions:
+            dt = "[{}] {}".format(d['POS'], d['DEF'])
+            ds.append(dt)
+        return '\n'.join(ds)
+
+    def example_text(self):
+        es = []
+        random_examples = self.examples
+        if len(self.examples) >= 3:
+            random_examples = random.sample(random_examples, 3)
+        for i, e in enumerate(random_examples):
+            et = "[{}] {}".format(i+1, e['EN'])
+            es.append(et)
+        return '\n'.join(es)
+
+class DictExtractor(object):
+    def __init__(self):
+        self.url = 'https://www.bing.com/dict/search?q={word}'
+
+    # 提取音标
+    def extract_pronunciation(self, soup):
+        pronunciations = []
+        us_pron = soup.find('div', class_='hd_prUS')
+        uk_pron = soup.find('div', class_='hd_pr')
+        pattern = r'\[(.*?)\]'
+        if us_pron:
+            us_pron_text = us_pron.get_text(strip=True)
+            match = re.search(pattern, us_pron_text)
+            if match:
+                pronunciations.append({'US': match.group(1)})
+        if uk_pron:
+            uk_pron_text = uk_pron.get_text(strip=True)
+            match = re.search(pattern, uk_pron_text)
+            if match:
+                pronunciations.append({'UK': match.group(1)})
+        return pronunciations
+
+    # 提取词义
+    def extract_definitions(self, soup):
+        definitions = []
+        pos_spans = soup.find_all('span', class_='pos')
+        def_spans = soup.find_all('span', class_='def')
+        for pos, definition in zip(pos_spans, def_spans):
+            pos_text = pos.get_text()
+            def_text = definition.get_text()
+            definitions.append({'POS': pos_text, 'DEF': def_text})
+        return definitions
+
+    # 提取例句
+    def extract_examples(self, soup):
+        examples = []
+        en_examples = soup.find_all('div', class_='sen_en')
+        cn_examples = soup.find_all('div', class_='sen_cn')
+        for en, cn in zip(en_examples, cn_examples):
+            en_text = en.get_text()
+            cn_text = cn.get_text()
+            examples.append({'EN': en_text, 'CN': cn_text})
+        return examples
+
+    # 提取单词信息
+    def extract_word(self, word):
+        word_info = WordInfo(word)
+
+        url = self.url.format(word=word)
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Error response: {response.status_code}")
+            return word_info
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        word_info.pronunciations = self.extract_pronunciation(soup)
+        word_info.definitions = self.extract_definitions(soup)
+        word_info.examples = self.extract_examples(soup)
+        return word_info
+
+# 测试示例
+if __name__ == '__main__':
+    word = 'within'
+    bing_dict = DictExtractor()
+    word_info = bing_dict.extract_word(word)
+    print(word_info.word)
+    print(word_info.word_text())
+    print(word_info.pronunciations)
+    print(word_info.pronunciation_text())
+    print(word_info.definitions)
+    print(word_info.definition_text())
+    print(word_info.examples)
+    print(word_info.example_text())
---
+++ scraper/bing/gen_xlsx.py
@@ -0,0 +1,83 @@
+import openpyxl
+import os
+import time
+import bing_dict
+
+def convert_txt_to_xlsx(txt_file, xlsx_file):
+    # 打开txt文件并读取单词列表
+    with open(txt_file, 'r', encoding='utf-8') as txt:
+        word_list = txt.read().splitlines()
+
+    # 如果有进度文件，从上次进度继续，否则从头开始
+    progress_file_name = f"{xlsx_file}.process"
+    if os.path.exists(progress_file_name):
+        workbook = openpyxl.load_workbook(xlsx_file)
+        with open(progress_file_name, 'r') as progress_file:
+            progress = int(progress_file.read())
+    else:
+        workbook = openpyxl.Workbook()
+        progress = 0
+
+    print(f"Last process: {progress}")
+
+    # 创建词典提取器
+    de = bing_dict.DictExtractor()
+
+    # 设置标题行
+    worksheet = workbook.active
+    worksheet['A1'] = '单词'
+    worksheet['B1'] = '音标'
+    worksheet['C1'] = '释义'
+    worksheet['D1'] = '评分'
+    worksheet['E1'] = '例句'
+
+    # 创建一个 Alignment 对象，设置对齐方式
+    alignment = openpyxl.styles.Alignment(horizontal='left', vertical='center', wrap_text=False)
+                    
+    # 将单词列表写入Excel表格
+    for i, word in enumerate(word_list):
+        seq = i + 1
+
+        # 从上次进度开始
+        if seq <= progress:
+            continue
+
+        print(f'Excel: {seq}/{len(word_list)} {word}')
+
+        word_info = de.extract_word(word)
+        worksheet.cell(row=seq+1, column=1, value=word_info.word_text()).alignment = alignment
+        worksheet.cell(row=seq+1, column=2, value=word_info.pronunciation_text()).alignment = alignment
+        worksheet.cell(row=seq+1, column=3, value=word_info.definition_text()).alignment = alignment
+        worksheet.cell(row=seq+1, column=4, value=1).alignment = alignment
+        worksheet.cell(row=seq+1, column=5, value=word_info.example_text()).alignment = alignment
+
+        # 分批次保存一下，在有大量数据情况下，有时异常中断时，就不用完全从头开始
+        if seq % 10 == 0:
+            # 保存数据文件
+            workbook.save(xlsx_file)
+            # 记录当前进度
+            with open(progress_file_name, 'w') as progress_file:
+                progress_file.write(str(seq))
+
+        # 分批次休息一下，尽量避免连续大量请求
+        if seq % 100 == 0:
+            time.sleep(5)
+
+    # 完整结束后，保存文件，并清除进度文件
+    workbook.save(xlsx_file)
+    os.remove(progress_file_name)
+
+if __name__ == '__main__':
+    txt_file = 'input.txt'  # 请将文件名替换为您的txt文件名
+    xlsx_file = 'output.xlsx'  # 请将文件名替换为您的xlsx文件名
+    retry_max = 10 # 遇到异常最大重试次数
+
+    # 执行功能，遇到异常自动重试，直到达到最大次数
+    for retry_count in range(retry_max):
+        try:
+            result = convert_txt_to_xlsx(txt_file, xlsx_file)
+            print(f"[Info] 完成")
+            break
+        except Exception as e:
+            print(f"[Warning] 尝试 {retry_count + 1} 出现异常: {str(e)}")
+ 
