6ec3953257181d321ef7cee0f93086a3515ef592
kaleofeng <kaleofeng@gmail.com>
2024-02-12 20:42:47 +0800

    Update scripts

--- scraper/bing/gen_xlsx.py
+++ scraper/bing/gen_xlsx.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#  -*- coding:utf-8 -*-
+
 import openpyxl
 import os
 import time
--- tool/rename/rename.sh
+++ tool/rename/rename.sh
@@ -11,4 +11,13 @@ src=$1
 dst=$2
 dir=$3
 
-ls $dir | awk "{x=\$0; gsub(/$src/, \"$dst\"); print \"mv \"x\" \"\$0}" | sh
+#ls $dir | awk "{x=\$0; gsub(/$src/, \"$dst\"); print \"mv \"x\" \"\$0}" | sh
+
+for file in "$dir"/*; do
+    new_name=$(echo "$file" | awk -v src="$src" -v dst="$dst" '{new=gensub(src, dst, "g"); if (new != $0) print new}')
+    if [ -n "$new_name" ]; then
+        cmd="mv '$file' '$new_name'"
+        echo "$cmd"
+        eval "$cmd"
+    fi
+done
